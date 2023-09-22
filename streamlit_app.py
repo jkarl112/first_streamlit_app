3a41734de84d893bf0be0df3fb12d326a76e9525
John Karl Auxteriaco <48265551+jkarl112@users.noreply.github.com>
2023-09-22 16:27:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blue Berry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie\n')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard Boiled Free Ranged Egg')
