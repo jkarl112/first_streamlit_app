f63b5452bb323fd195389e78d3b51b222432dc66
John Karl Auxteriaco <48265551+jkarl112@users.noreply.github.com>
2023-09-28 14:40:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 except URLError as e:
-  streamlit.error()
+  streamlit.error('Not Found')
     
     
 #streamlit.write('The user entered', fruit_choice)
