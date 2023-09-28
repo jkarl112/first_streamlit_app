19689e4135c5eab779171fde73d6d581a6d6b91e
John Karl Auxteriaco <48265551+jkarl112@users.noreply.github.com>
2023-09-28 10:05:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruit_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.text('The user entered '+ fruit_choice)
+streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
